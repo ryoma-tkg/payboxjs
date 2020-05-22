ea7e493ee6eed3e46daba2c51191e8f7e46c98dc
YukiOkano <oyuuki0816@yahoo.co.jp>
2020-05-22 23:11:46 +0900

    設定画面各ボタンに<br>

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -6,16 +6,16 @@
       <img src="プロフィール写真" alt="プロフィール写真">
       <div>"アカウント名"</div>
       <div>
-         <button type="button"><img src="" alt="アカウント情報ボタン"></button>
-         <button type="button"><img src="" alt="支払い管理ボタン"></button>
-         <button type="button"><img src="" alt="通知設定ボタン"></button>
-         <button type="button"><img src="" alt="セキュリティボタン"></button>
+         <button type="button"><img src="" alt="アカウント情報ボタン"></button><br>
+         <button type="button"><img src="" alt="支払い管理ボタン"></button><br>
+         <button type="button"><img src="" alt="通知設定ボタン"></button><br>
+         <button type="button"><img src="" alt="セキュリティボタン"></button><br>
       </div>
       <div>
-         <button type="button"><img src="" alt="このアプリについてボタン"></button>
-         <button type="button"><img src="" alt="ヘルプボタン"></button>
-         <button type="button"><img src="" alt="友達に紹介ボタン"></button>
-         <button type="button"><img src="" alt="問い合わせボタン"></button>
+         <button type="button"><img src="" alt="このアプリについてボタン"></button><br>
+         <button type="button"><img src="" alt="ヘルプボタン"></button><br>
+         <button type="button"><img src="" alt="友達に紹介ボタン"></button><br>
+         <button type="button"><img src="" alt="問い合わせボタン"></button><br>
       </div>
          <button type="button"><img src="" alt="ログアウトボタン"></button>
    </div>
