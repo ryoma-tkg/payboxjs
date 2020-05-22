c328a0f8ecb7c0fb8e3061fae0c7f1710fe4be36
YukiOkano <oyuuki0816@yahoo.co.jp>
2020-05-22 16:44:34 +0900

    設定画面_リンクをボタンに編集

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -1,25 +1,24 @@
 <template>
    <div>
-      <p>設定</p>
+      <div>
+         <p>設定</p>
+      </div>
+      <img src="プロフィール写真" alt="プロフィール写真">
+      <div>"アカウント名"</div>
+      <div>
+         <div id="acount_info"><button v-on:click="遷移処理">アカウント情報</button></div>
+         <div id="pay_manager"><button v-on:click="遷移処理">支払い管理</button></div>
+         <div id="notice_setting"><button v-on:click="遷移処理">通知設定</button></div>
+         <div id="security"><button v-on:click="遷移処理">セキュリティ</button></div>
+      </div>
+      <div>
+         <div id="about_app"><button v-on:click="遷移処理">このアプリについて</button></div>
+         <div id="help"><button v-on:click="遷移処理">ヘルプ</button></div>
+         <div id="prom_friends"><button v-on:click="遷移処理">友達に紹介</button></div>
+         <div id="query"><button v-on:click="遷移処理">問い合わせ</button></div>
+      </div>
+         <div id="log_out"><button v-on:click="遷移処理">ログアウト</button></div>
    </div>
-   <img src="プロフィール写真" alt="プロフィール写真">
-   <div>
-      "アカウント名"
-   </div>
-   <div>
-      <p>アカウント情報</p>
-      <p>支払い管理</p>
-      <p>通知設定</p>
-      <p>セキュリティ</p>
-   </div>
-   <div>
-      <p>このアプリについて</p>
-      <p>ヘルプ</p>
-      <p>友達に紹介</p>
-      <p>問い合わせ</p>
-   </div>
-   <div>ログアウト</div>
-   
 </template>
 
 <script>
