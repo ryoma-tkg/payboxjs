ab0bc17b90cede2212eebe93f93f96a3d36875f6
YukiOkano <oyuuki0816@yahoo.co.jp>
2020-05-22 23:25:55 +0900

    設定画面ボタン遷移完成

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -6,18 +6,18 @@
       <img src="プロフィール写真" alt="プロフィール写真">
       <div>"アカウント名"</div>
       <div>
-         <button type="button"><img src="" alt="アカウント情報ボタン"></button><br>
-         <button type="button"><img src="" alt="支払い管理ボタン"></button><br>
-         <button type="button"><img src="" alt="通知設定ボタン"></button><br>
-         <button type="button"><img src="" alt="セキュリティボタン"></button><br>
+         <router-link to="/setting/acountInfo"><button type="button"><img src="" alt="アカウント情報ボタン"></button></router-link><br>
+         <router-link to="/setting/payManager"><button type="button"><img src="" alt="支払い管理ボタン"></button></router-link><br>
+         <router-link to="/setting/notice"><button type="button"><img src="" alt="通知設定ボタン"></button></router-link><br>
+         <router-link to="/setting/security"><button type="button"><img src="" alt="セキュリティボタン"></button></router-link><br>
       </div>
       <div>
-         <button type="button"><img src="" alt="このアプリについてボタン"></button><br>
-         <button type="button"><img src="" alt="ヘルプボタン"></button><br>
-         <button type="button"><img src="" alt="友達に紹介ボタン"></button><br>
-         <button type="button"><img src="" alt="問い合わせボタン"></button><br>
+         <router-link to="/setting/applicationInfo"><button type="button"><img src="" alt="このアプリについてボタン"></button></router-link><br>
+         <router-link to="/setting/help"><button type="button"><img src="" alt="ヘルプボタン"></button></router-link><br>
+         <router-link to="/setting/promoFriends"><button type="button"><img src="" alt="友達に紹介ボタン"></button></router-link><br>
+         <router-link to="/setting/question"><button type="button"><img src="" alt="問い合わせボタン"></button></router-link><br>
       </div>
-         <button type="button"><img src="" alt="ログアウトボタン"></button>
+         <router-link to="/setting/logOut"><button type="button"><img src="" alt="ログアウトボタン"></button></router-link>
    </div>
 </template>
 
