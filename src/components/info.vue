89d1ddb1ef7b96c7456b9b66520137fd8b2c9409
YukiOkano <oyuuki0816@yahoo.co.jp>
2020-05-25 19:14:53 +0900

    インフォメーション画面_開発

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,5 +1,11 @@
 <template>
-   <p>お知らせ</p>
+  <div>
+    <p>お知らせ</p>
+    <router-link to="/info/newsystem"><button type="button"><img src="" alt="新機能"></button></router-link><br>
+    <router-link to="/info/recommend"><button type="button"><img src="" alt="おすすめアプリ"></button></router-link><br>
+    <router-link to="/info/campaign"><button type="button"><img src="" alt="現在のキャンペーン"></button></router-link><br>
+    <router-link to="/info/maintenance"><button type="button"><img src="" alt="メンテナンス"></button></router-link><br>
+  </div>
 </template>
 
 <script>
