12ee2c8a44dbd4bfc548092df7a36e4eba9f86bf
yosipy <todacchi.desu@gmail.com>
2020-05-28 18:51:16 +0900

    build test

--- src/components/home.vue
+++ src/components/home.vue
@@ -133,7 +133,7 @@ export default {
         if (tmp['name'] === 'クレジットカード') {
           tmp['imgPathBig'] = './static/test/home_kessai/card.svg'
           tmp['imgPathSmall'] = './static/test/kessai/card.svg'
-          tmp['link'] = '#'
+          tmp['link'] = 'javascript:void(0)'
           tmp['label'] = 'クレジットカードで支払ってください'
         } else if (tmp['name'] === 'PayPay(現金チャージ)') {
           tmp['imgPathBig'] = './static/test/home_kessai/paypay.svg'
@@ -143,7 +143,7 @@ export default {
         } else if (tmp['name'] === 'visa LINE Payカード') {
           tmp['imgPathBig'] = './static/test/home_kessai/visa_line.svg'
           tmp['imgPathSmall'] = './static/test/kessai/visa_line.svg'
-          tmp['link'] = '#'
+          tmp['link'] = 'javascript:void(0)'
           tmp['label'] = 'visa LINE Payカードで支払ってください'
         } else if (tmp['name'] === 'LINE Pay') {
           tmp['imgPathBig'] = './static/test/home_kessai/line.svg'
@@ -173,7 +173,7 @@ export default {
         } else if (tmp['name'] === 'Kyash Card') {
           tmp['imgPathBig'] = './static/test/home_kessai/kyash.svg'
           tmp['imgPathSmall'] = './static/test/kessai/kyash.svg'
-          tmp['link'] = '#'
+          tmp['link'] = 'javascript:void(0)'
           tmp['label'] = 'Kyash Cardで支払ってください'
         }
 
