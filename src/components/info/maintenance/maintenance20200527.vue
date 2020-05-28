c95aff53d5898ff9e3d992243a57ca28e54f4b9e
pg-tomoda <tomoda@gmo-pg.com>
2020-05-28 14:21:25 +0900

    infoフォルダー編集とセキュリティー修正

--- src/components/info/maintenance/maintenance20200527.vue
+++ src/components/info/maintenance/maintenance20200527.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+  <div class="title">
+    <h1>メンテナンス情報</h1>
+  </div>
+      <p>現在メンテナンス情報はありません</p>
   </div>
 </template>
 
@@ -7,4 +11,34 @@
 </script>
 
 <style>
+.title{
+  height: 45px;
+  width: 100%;
+  background-color: #FFF;
+  text-align: center;
+  padding-top: 25px;
+  color: #626262;
+  font-size: 10px;
+
+  border-radius: 0 0 30px 30px;
+
+  margin-bottom: 30px;
+
+  position: fixed;
+  top: 0px;
+
+  -webkit-box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+}
+
+h1{
+  padding-right: 10px;
+  text-align: center;
+}
+
+p{
+  position: relative;
+  top: 50px;
+}
 </style>
--- src/components/info/newsystem/newsystem20200527.vue
+++ src/components/info/newsystem/newsystem20200527.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="title">
+    <h1>新機能</h1>
+    </div>
+    <div class="wrap">
+      <p>検索可能な決済手段に、「Visa LINE Payクレジットカード」が追加されました。</p>
+    </div>
   </div>
 </template>
 
@@ -7,4 +13,47 @@
 </script>
 
 <style>
+.title{
+  height: 45px;
+  width: 100%;
+  background-color: #FFF;
+  text-align: center;
+  padding-top: 25px;
+  color: #626262;
+  font-size: 10px;
+
+  border-radius: 0 0 30px 30px;
+
+  margin-bottom: 30px;
+
+  position: fixed;
+  top: 0px;
+
+  -webkit-box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 5px 50px 0px rgba(0, 0, 0, 0.2);
+}
+
+h1{
+  padding-right: 10px;
+  text-align: center;
+}
+
+.wrap{
+   width: 90%;
+   margin:0 auto;
+}
+
+p{
+  position: relative;
+  top: 40px;
+   padding:20px;
+   border-radius: 20px;
+   background-color: #FFF;
+   -webkit-box-shadow: 0px 5px 30px 0px rgba(0,0,0,0.3);
+   -moz-box-shadow: 0px 5px 30px 0px rgba(0,0,0,0.3);
+   box-shadow: 0px 5px 30px 0px rgba(0,0,0,0.3);
+
+   margin:15px 0 15px 0;
+}
 </style>
